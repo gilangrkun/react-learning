2f8d7d368be2dbb7386f4eb51022d2404a3b6a94
kuntiarso <gilang.grk17@gmail.com>
2020-09-18 13:24:11 +0700

    first commit | Tailwind.css

---
+++ react-tailwind/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h4>Learning TailwindCSS</h4>
+      <p>
+        Let me use this css framework for building my own portfolio. I'm going
+        now!
+      </p>
+    </div>
+  );
+}
+
+export default App;
